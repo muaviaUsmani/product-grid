0d715c78987923152b180e12b8ebf14e0ed78677
Muavia Usmani <meusmani@yahoo.com>
2020-09-12 13:50:43 +0500

    setup routing, add fonts and bootstrap, setup basic template

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import './App.scss';
 
+import { Redirect, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+
+import Products from './pages/Products/Products';
 import React from 'react';
+
+enum Routes {
+  Products = "/",
+  NotFound = "/404"
+};
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Switch>
+        <Route path={Routes.Products} exact={true} component={Products} />
+        <Route path={Routes.NotFound} exact={true} component={Products} />
+
+        <Redirect to={Routes.NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
