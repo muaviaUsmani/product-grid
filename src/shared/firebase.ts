a5b9536309302e82c5c986a9594ce7fa9be1a960
Muavia Usmani <meusmani@yahoo.com>
2020-09-13 14:56:00 +0500

    add product grid, setup firebase storage, add new product modal

--- src/shared/firebase.ts
+++ src/shared/firebase.ts
@@ -3,13 +3,13 @@ import 'firebase/storage';
 import firebase from 'firebase/app';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBYly9_WAtDWTDv-R7ZTOUVUtQinBnln8o",
-  authDomain: "productgrid-8bc97.firebaseapp.com",
-  databaseURL: "https://productgrid-8bc97.firebaseio.com",
-  projectId: "productgrid-8bc97",
-  storageBucket: "productgrid-8bc97.appspot.com",
-  messagingSenderId: "563362281028",
-  appId: "1:563362281028:web:c17cada23e86f097b56e11"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MSG_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
